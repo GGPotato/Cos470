5a0b431bdb7e05076a8650fee28a703d69609129
GGPotato <54608858+GGPotato@users.noreply.github.com>
2019-10-08 18:26:54 -0400

    Update: dependinces, added a unit test and made files interconnected, Files now run. WHY:??

---
+++ Assignment 2/WebRequest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WebRequest
+{
+
+    using System;
+    using System.Net.Http;
+    using System.Web;
+
+    public class WebRequest
+    {
+        public static string DoStuff()
+        {
+            // Using HttpUtility to generate the parameters for the query string handles HTTP escaping the values.
+            // Change these:
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+            parameters["where"] = "MUNICIPALITY='Lewiston'";
+            parameters["outFields"] = "ADDRESS_NUMBER,STREETNAME,SUFFIX,MUNICIPALITY,Latitude,longitude";
+            parameters["f"] = "pjson";
+            parameters["resultRecordCount"] = "10";
+
+            //
+            var address = @"https://gis.maine.gov/arcgis/rest/services/Location/Maine_E911_Addresses_Roads_PSAP/MapServer/1/query?"
+                + parameters;
+
+            // Disposing HttpClient is not best practice. It's good enough and I'm keeping this simple.
+            using (var client = new HttpClient())
+            {
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, address))
+                {
+                    // .Result from an Async function is not best practice. It's good enough and I'm keeping this simple.
+                    var response = client.SendAsync(request).Result;
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new Exception($"{content}: {response.StatusCode}");
+                    }
+                    return content; // the content of the response
+                }
+            }
+        }
+    }
+}
