6a19abbc3b556541cd863f757e451f2a0787c2e5
GGPotato <54608858+GGPotato@users.noreply.github.com>
2019-10-08 18:33:11 -0400

    Added an example website.

---
+++ Website/TestWebPage/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestWebPage.Models;
+
+namespace TestWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ViewBag will not throw an error for type untill it crashes
+            //will take any properitys you give it 
+            //DO NOT USE THIS!
+            //ViewBag.ToString(); // ViewBag 
+            //var x = ViewBag + 4; //ViewBag
+            //ViewBag.Dispose();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Website/TestWebPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestWebPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
